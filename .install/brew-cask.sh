f047235276cf17852338d8f1855fac4765494100
Daniel Saul <daniel@dansaul.co.uk>
2017-05-21 21:49:03 +0100

    Brew beginnings

---
+++ .install/brew-cask.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Caskroom
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
